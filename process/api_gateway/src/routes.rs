d4b80646c95475ada852113661fa555a055b6a6c
blackh-t <nache7383@uit.no>
2026-07-18 22:37:19 +0200

    refactor: seperated scopes into groups

--- process/api_gateway/src/routes.rs
+++ process/api_gateway/src/routes.rs
@@ -1,31 +1,37 @@
+use actix_web::web::{self};
+
+use crate::constants;
 use crate::handlers::contracts::Contracts;
 use crate::handlers::transaction::Transaction;
 use crate::handlers::user::auth::AuthController;
-use actix_web::web::{self};
 
 pub fn api_routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::scope("/api")
-            .service(
-                web::scope("/user")
-                    .service(
-                        web::resource("/auth")
-                            .route(web::get().to(AuthController::request_challenge))
-                            .route(web::post().to(AuthController::login)),
-                    )
-                    .route("/logout", web::post().to(AuthController::logout))
-                    .route("/verify", web::post().to(AuthController::verify_session)),
-            )
-            .service(
-                web::scope("/transactions")
-                    .route("/deposit", web::post().to(Transaction::deposit))
-                    .route("", web::get().to(Transaction::list)),
-            )
-            .service(
-                web::scope("/contracts")
-                    .route("/nonce", web::get().to(Contracts::get_nonce))
-                    .route("/sign", web::post().to(Contracts::sign)),
-            )
-            .route("/config", web::get().to(crate::constants::get_config)),
+    cfg.service(web::scope("/api")
+        .service(user_routes())
+        .service(transaction_routes())
+        .service(contract_routes())
+        .route("/config", web::get().to(constants::get_config))
     );
+}
+
+fn user_routes() -> web::Scope {
+    web::scope("/user")
+        .service(web::resource("/auth")
+            .route(web::get().to(AuthController::request_challenge))
+            .route(web::post().to(AuthController::login))
+        )
+        .route("/logout", web::post().to(AuthController::logout))
+        .route("/verify", web::post().to(AuthController::verify_session))
+}
+
+fn transaction_routes() -> web::Scope {
+    web::scope("/transactions")
+        .route("/deposit", web::post().to(Transaction::deposit))
+        .route("", web::get().to(Transaction::list))
+}
+
+fn contract_routes() -> web::Scope {
+    web::scope("/contracts")
+        .route("/nonce", web::get().to(Contracts::get_nonce))
+        .route("/sign", web::post().to(Contracts::sign))
 }
